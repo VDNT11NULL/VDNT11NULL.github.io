c9683c0ea8ffa0fa34edec48a7cae54af996299f
VDNT11NULL <vedantmarodkar11@gmail.com>
2024-09-29 22:31:39 +0530

    JS updated

--- js/custom.js
+++ js/custom.js
@@ -1,47 +1,59 @@
 (function ($) {
-
   "use strict";
 
-  //--------VDNT change
-      $(document).ready(function() {
-        if (localStorage.getItem('darkMode') === 'enabled') {
-            $('body').addClass('dark-mode');
-            $('.color-mode-icon').addClass('active'); // Optional: Change the icon to active
-        } else {
-            $('body').removeClass('dark-mode');
-        }
-      });
-//-------------------
+  $(document).ready(function() {
+      // Check if darkMode is set in localStorage
+      if (localStorage.getItem('darkMode') === null) {
+          // If it doesn't exist, set darkMode to 'enabled' for default dark mode
+          localStorage.setItem('darkMode', 'enabled');
+      }
 
-    // COLOR MODE
-    $('.color-mode').click(function(){
-      $('body').toggleClass('dark-mode')
-        $('.color-mode-icon').toggleClass('active')
-    })
+      // Apply dark mode based on the value in localStorage
+      if (localStorage.getItem('darkMode') === 'enabled') {
+          $('body').addClass('dark-mode');
+          $('.color-mode-icon').addClass('active'); // Optional: Change the icon to active
+      } else {
+          $('body').removeClass('dark-mode');
+      }
+  });
 
-    // HEADER
-    $(".navbar").headroom();
+  // COLOR MODE
+  $('.color-mode').click(function() {
+      // Toggle dark mode on click
+      $('body').toggleClass('dark-mode');
+      $('.color-mode-icon').toggleClass('active');
 
-    // PROJECT CAROUSEL
-    $('.owl-carousel').owlCarousel({
-    	items: 1,
-	    loop:true,
-	    margin:10,
-	    nav:true
-	});
+      // Update localStorage based on the new state
+      if ($('body').hasClass('dark-mode')) {
+          localStorage.setItem('darkMode', 'enabled');
+      } else {
+          localStorage.setItem('darkMode', 'disabled');
+      }
+  });
 
-    // SMOOTHSCROLL
-    $(function() {
+  // HEADER
+  $(".navbar").headroom();
+
+  // PROJECT CAROUSEL
+  $('.owl-carousel').owlCarousel({
+      items: 1,
+      loop: true,
+      margin: 10,
+      nav: true
+  });
+
+  // SMOOTH SCROLL
+  $(function() {
       $('.nav-link, .custom-btn-link').on('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - 49
-        }, 1000);
-        event.preventDefault();
+          var $anchor = $(this);
+          $('html, body').stop().animate({
+              scrollTop: $($anchor.attr('href')).offset().top - 49
+          }, 1000);
+          event.preventDefault();
       });
-    });  
+  });  
 
-    // TOOLTIP
-    $('.social-links a').tooltip();
+  // TOOLTIP
+  $('.social-links a').tooltip();
 
 })(jQuery);
